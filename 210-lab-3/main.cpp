86703d7d5ff9e86e16a3279a6020dbdb3b698a26
tanmayeet <62356243+tanmayeet@users.noreply.github.com>
2025-09-07 13:01:38 -0700

    Delete 210-lab-3/main.cpp
    getting confusing

--- 210-lab-3/main.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream> // Includes the input/output stream library
-using namespace std;
-
-int main() { // The main function, where program execution begins
-    std::cout << "Hello World!" << std::endl; // Prints "Hello World!" to the console
-    return 0; // Indicates successful program execution
-}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
