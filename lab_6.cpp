ed606de5cef2f1963116f4c9a88b0fd70936fdf6
Tanmayee Chalamalasetti <tanmayeesc@Tanmayees-MacBook-Pro-190.local>
2025-09-14 16:32:38 -0700

    push 4

--- lab_6.cpp
+++ lab_6.cpp
@@ -1,24 +1,32 @@
 // COMSC 210 | Lab 6 | Tanmayee Chalamalasetti
 // IDE used: VS Code
 
-#include <ctime>
 #include <iostream>
 using namespace std;
 
-int enterArrayData(double* arr, int size) {
-  double* arr[size] = new double[size];
+// Function prototypes
+int enterArrayData(double* arr, int size);
+int outputArrayData(double* arr, int size);
+void sumArray(double* arr, int size);
+
+void enterArrayData(double* arr, int size) {
+  double* array[size] = new double[size];
   cout << "Data entry for the array:" << endl;
 
   for (int i = 0; i < size; i++) {
     cout << "Element #" << i << ":";
-    cin << arr[i] << endl;
+    cin >> *(arr + i);
   }
 }
 
-int outputArrayData(double* arr, int size) {}
+void outputArrayData(double* arr, int size) {
+  double* arr[size] = new double[size];
+}
 
-void sumArray(double* arr, int size) {
+double sumArray(double* arr, int size) {
+  double* arr[size] = new double[size];
   int sum = 0;
+
   for (int i = 0; i < size; i++) {
     sum = arr[i]
   }
