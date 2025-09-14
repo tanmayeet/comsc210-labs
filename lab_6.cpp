988e1878e5e63580929cb8861be92a8a0c2e8e89
Tanmayee Chalamalasetti <tanmayeesc@Tanmayees-MacBook-Pro-190.local>
2025-09-14 16:38:54 -0700

    push 5

--- lab_6.cpp
+++ lab_6.cpp
@@ -5,35 +5,30 @@
 using namespace std;
 
 // Function prototypes
-int enterArrayData(double* arr, int size);
-int outputArrayData(double* arr, int size);
-void sumArray(double* arr, int size);
+void enterArrayData(double* arr, int size);
+void outputArrayData(double* arr, int size);
+double sumArray(double* arr, int size);
 
+//
 void enterArrayData(double* arr, int size) {
-  double* array[size] = new double[size];
   cout << "Data entry for the array:" << endl;
-
-  for (int i = 0; i < size; i++) {
-    cout << "Element #" << i << ":";
-    cin >> *(arr + i);
+  for (int i = 0; i < size; i++) {  // iterates through array
+    cout << "Element #" << i << ": ";
+    cin >> *(arr + i);  // using pointer notation instead of array notation
   }
 }
 
-void outputArrayData(double* arr, int size) {
-  double* arr[size] = new double[size];
-}
+void outputArrayData(double* arr, int size) {}
 
 double sumArray(double* arr, int size) {
-  double* arr[size] = new double[size];
   int sum = 0;
-
   for (int i = 0; i < size; i++) {
-    sum = arr[i]
+    sum += *(arr + i);
   }
+  return sum;
 }
-int main(
 
-    const int size = 5;
+int main(const int size = 5;
 
 ) {
   return 0;
