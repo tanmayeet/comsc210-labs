7240b21f55912bcbac7fe8b2916dd6739dd93a01
Tanmayee Chalamalasetti <tanmayeesc@Tanmayees-MacBook-Pro-190.local>
2025-09-14 14:53:10 -0700

    I had some git issues and couldn't push within the 10 minutes

---
+++ lab_6.cpp
@@ -0,0 +1,19 @@
+// COMSC 210 | Lab 6 | Tanmayee Chalamalasetti
+// IDE used: VS Code
+
+#include <ctime>
+#include <iostream>
+using namespace std;
+
+int enterArrayData(/*whtvr is in here*/) {
+  cout << "Data entry for the array:" << endl;
+  for (int i = 0; i < SIZE; i++) {
+  }
+  cout << "Element #" <<
+}
+
+int outputArrayData() {}
+
+void sumArray() {}
+
+int main() { return 0; }
