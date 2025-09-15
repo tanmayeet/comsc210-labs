db3c93b2387445442cf41deb8f26b047a75d2858
Tanmayee Chalamalasetti <tanmayeesc@Tanmayees-MacBook-Pro-190.local>
2025-09-14 17:50:25 -0700

    push 2

---
+++ lab_7.cpp
@@ -0,0 +1,22 @@
+// COMSC 210 | Lab 6 | Tanmayee Chalamalasetti
+// IDE used: VS Code
+
+#include <iostream>
+using namespace std;
+
+void reverseArray(string* arr, int size) {
+  cout << "Original array:" << endl;
+
+  for (int i = 0; i < size; i++) {  // iterates through array
+    arr[i]
+  }
+}
+
+string displayArray(string* arr, int size) {}
+
+int main() {
+  const int size = 5;
+  string* arr = new string[size];
+
+  return 0;
+}
